b760f0b69cab0d797dc10b9f729bbf3a0ac08bac
Kasthoori <ap.kasth@gmail.com>
2024-06-06 17:54:42 +1200

    add customer service mapper repository classes

---
+++ services/customer/src/main/java/org/learning/ecommerce/customer/CustomerRequest.java
@@ -0,0 +1,20 @@
+package org.learning.ecommerce.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record CustomerRequest(
+		 String id,
+
+		 @NotNull(message = "Customer firstname is required")
+		 String firstName,
+
+		 @NotNull(message = "Customer lastname is required")
+		 String lastName,
+
+		 @NotNull(message = "Customer email is required")
+		 @Email(message = "Customer email is not valid email")
+		 String email,
+		 Address address
+) {
+}
